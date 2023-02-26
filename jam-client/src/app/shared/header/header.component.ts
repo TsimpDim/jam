1467030da945b9f3520931f3e3c8702e4a81cc1b
TsimpDim <dimitristsimpitas@gmail.com>
2023-02-26 19:02:54 +0100

    Add global header

--- jam-client/src/app/shared/header/header.component.ts
+++ jam-client/src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
@@ -6,13 +8,28 @@ import { AuthService } from 'src/app/_services/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss', '../shared.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   public isLoggedIn: Boolean | null = null;
-  @Input() showSubMenu: boolean = true;
-  
-  constructor(private authService: AuthService) {}
+  routerSubscription: Subscription | undefined;
+
+  constructor(private authService: AuthService, private router: Router) {}
+  ngAfterViewInit(): void {
+    this.routerSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.checkIsLoggedIn();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription?.unsubscribe();
+  }
 
   ngOnInit(): void {
+    this.checkIsLoggedIn();
+  }
+
+  checkIsLoggedIn() {
     this.isLoggedIn = this.authService.getSessionToken() ? true : false;
   }
 
