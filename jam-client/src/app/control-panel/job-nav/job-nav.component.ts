b4af6716f39e434a4bc043fc598c2ac9d1f8126a
TsimpDim <dimitristsimpitas@gmail.com>
2026-03-22 21:26:17 +0100

    Add re-ordering of groups

--- jam-client/src/app/control-panel/job-nav/job-nav.component.ts
+++ jam-client/src/app/control-panel/job-nav/job-nav.component.ts
@@ -1,30 +1,40 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-job-nav',
   templateUrl: './job-nav.component.html',
   styleUrls: ['./job-nav.component.scss']
 })
-export class JobNavComponent {
+export class JobNavComponent implements OnInit {
   @Input() applications: any = null;
   @Input() loadingApplications: boolean = false;
   @Output() onSelectApp = new EventEmitter();
   @Output() onOpenAndClearJobAppModal = new EventEmitter();
-  static navState: any = {};
+  static lastOpenedGroup: string | null = null;
 
   keepOriginalOrder = (a: any, b: any) => a.key;
 
-  toggleNavState(event: any, groupName: any) {
-    event.stopPropagation();
-
-    if (JobNavComponent.navState[groupName] !== undefined) {
-      JobNavComponent.navState[groupName] = !JobNavComponent.navState[groupName];
-    } else {
-      JobNavComponent.navState[groupName] = true;
+  ngOnInit() {
+    const saved = localStorage.getItem('jam_last_opened_group');
+    if (saved && this.applications && saved in this.applications) {
+      JobNavComponent.lastOpenedGroup = saved;
+    } else if (this.applications) {
+      JobNavComponent.lastOpenedGroup = Object.keys(this.applications)[0] || null;
     }
   }
 
+  toggleNavState(event: any, groupName: any) {
+    event.stopPropagation();
+    JobNavComponent.lastOpenedGroup = groupName;
+    localStorage.setItem('jam_last_opened_group', groupName);
+  }
+
+  onAppSelect(groupName: string) {
+    JobNavComponent.lastOpenedGroup = groupName;
+    localStorage.setItem('jam_last_opened_group', groupName);
+  }
+
   getNavState(groupName: any) {
-    return JobNavComponent.navState[groupName];
+    return JobNavComponent.lastOpenedGroup === groupName;
   }
 }
