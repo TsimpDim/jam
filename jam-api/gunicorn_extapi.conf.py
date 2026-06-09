7cd6ec3ca7552e1a001d8d5e45bcf370ac54e538
TsimpDim <dimitristsimpitas@gmail.com>
2026-06-09 20:47:14 +0200

    Add separate gunicorn files

---
+++ jam-api/gunicorn_extapi.conf.py
@@ -0,0 +1,35 @@
+import os
+
+# Dynamic settings module (default to full)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.extapi")
+
+# Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
+wsgi_app = "core.asgi:application"
+
+# The granularity of Error log outputs
+loglevel = "info"
+
+# The number of worker processes for handling requests
+workers = 1
+
+# The socket to bind
+#bind = "/run/gunicorn.sock"
+bind = "0.0.0.0:8001"
+
+# Don't restart workers when code changes (development only!)
+reload = False
+
+# Write access and error info to /var/log
+accesslog = errorlog = "var/log/extapi.log"
+
+# Redirect stdout/stderr to log file
+capture_output = True
+
+# PID file so you can easily fetch process ID
+pidfile = "var/run/extapi.pid"
+
+# Daemonize the Gunicorn process (detach & enter background)
+daemon = True
+
+# ASGI Uvicorn worker class (instead of WSGI)
+worker_class = "uvicorn.workers.UvicornWorker"
